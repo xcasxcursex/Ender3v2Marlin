482384b3fc7e48a58336e515b15fb554930f382d
xcasxcursex <58130821+xcasxcursex@users.noreply.github.com>
2020-12-08 15:45:09 +1100

    Safer Bed Sizing
    232x232 - 3mm border around bed to ensure no nozzle hangups

--- Firmware/Configuration/Configuration.h
+++ Firmware/Configuration/Configuration.h
@@ -1133,8 +1133,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 232
+#define Y_BED_SIZE 232
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
